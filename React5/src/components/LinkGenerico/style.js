63e19f9f1ddc76172f44d5cda418e3110e035cfa
Mateus Padilha <103007201+padirex@users.noreply.github.com>
2023-10-30 15:30:08 -0300

    Delete React5 directory

--- React5/src/components/LinkGenerico/style.js
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-const MeuA = styled.a`
-    margin: 2em;
-`;
-
-export { MeuA };
--- React5/src/components/LinkSegundo.jsx
+++
@@ -1,3 +0,0 @@
-const LinkSegundo = () => <a href="segundo">Link para Segundo</a>;
-
-export default LinkSegundo;
